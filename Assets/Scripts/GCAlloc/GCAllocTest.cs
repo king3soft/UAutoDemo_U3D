09ba496720ba5c618fd8ac74f3c31190d3a5345e
huanghaiting <290525448@qq.com>
2021-02-18 11:13:53 +0800

    update base

---
+++ Assets/Scripts/GCAlloc/GCAllocTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GCAllocTest : MonoBehaviour
+{
+    private int[] m_GCAlloc = new int[10240];
+    private WaitForSeconds m_WFS;
+    private int i;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    
+}
