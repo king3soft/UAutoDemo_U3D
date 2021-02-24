0db2ee48c461fc45a5d935461128db2b2ee475af
huanghaiting <290525448@qq.com>
2021-02-24 11:01:17 +0800

    Update UAutoSdkInit.cs

--- EndlessRunDemo/Assets/UAutoSdkInit.cs
+++ EndlessRunDemo/Assets/UAutoSdkInit.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections;
 using UnityEngine;
-using UnityEngine.Profiling;
 using Debug = UnityEngine.Debug;
 namespace UAutoSDK
 {
@@ -23,30 +21,5 @@ namespace UAutoSDK
                 Debug.LogErrorFormat("[ERROR] UAutoSDk Reg MsgHandler Error. {0}", e.ToString());
             }
         }
-
-    private void Update()
-    {
-      if(Input.GetMouseButtonDown(0))
-      {
-        Profiler.SetAreaEnabled(0, true);
-        Profiler.SetAreaEnabled((ProfilerArea)2, true);
-        Profiler.SetAreaEnabled((ProfilerArea)1, true);
-        Profiler.SetAreaEnabled((ProfilerArea)3, true);
-        Profiler.SetAreaEnabled((ProfilerArea)6, true);
-        Profiler.SetAreaEnabled((ProfilerArea)10, true);
-        Profiler.SetAreaEnabled((ProfilerArea)11, true);
-        Profiler.SetAreaEnabled((ProfilerArea)12, true);
-        Profiler.logFile = Application.persistentDataPath + "/"+ "test";
-        Profiler.enableBinaryLog = true;
-        Profiler.enabled = true;
-        Profiler.maxUsedMemory = 1024 * 1024 * 1024;
-      }
-      if(Input.GetMouseButtonUp(0))
-      {
-        Profiler.enabled = false;
-        Profiler.logFile = "";
-        Profiler.enableBinaryLog = false;
-      }
-    }
   }
 }
